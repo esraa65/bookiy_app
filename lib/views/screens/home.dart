8cffb4fc8b98b364f2cf39df5ba9edbd24a4696a
esraa65 <esraakhaled6565@gmail.com>
2023-10-31 16:43:24 +0200

    best seller list view done

--- lib/views/screens/home.dart
+++ lib/views/screens/home.dart
@@ -27,7 +27,6 @@ class Home extends StatelessWidget {
                       style: Styles.textStyle18,
                     ),
                   ),
-                  // BestSellerListView()
                 ],
               ),
             ),
