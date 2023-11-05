c8942cdda5822f5e33ef4e363bd164a38c69dc81
Mahrous <Kareem.ali.sa@gmail.com>
2023-11-05 14:54:26 +0200

    book details view body done

--- lib/views/component/book_details_view_body.dart
+++ lib/views/component/book_details_view_body.dart
@@ -2,6 +2,7 @@ import 'package:booklyapp/core/styles.dart';
 import 'package:booklyapp/views/component/book_button.dart';
 import 'package:booklyapp/views/component/book_rating.dart';
 import 'package:booklyapp/views/component/list_view_item/custom_Book_Image.dart';
+import 'package:booklyapp/views/component/smaller_list_view.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
@@ -13,39 +14,68 @@ class BookDetailsViewBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var width = MediaQuery.of(context).size.width;
-    return Column(crossAxisAlignment: CrossAxisAlignment.center,
-      children: [
-        const CustomBookDetailsAppBar(),
-        Padding(
-          padding: EdgeInsets.symmetric(horizontal: width * .28),
-          child: const CustomBookImage(),
-        ),
-        // SizedBox(
-        //   height: 5.h,
-        // ),
-        Text(
-          'The Jungle Book',
-          style: Styles.textStyle20.copyWith(
-            fontWeight: FontWeight.bold,
-          ),
-        ),
-        SizedBox(
-          height: 7.h,
-        ),
-        Opacity(
-          opacity: .7,
-          child: Text(
-            'Rudyard Kip',
-            style: Styles.textStyle18.copyWith(
-                fontStyle: FontStyle.italic, fontWeight: FontWeight.w500),
-          ),
-
-        ),
-        SizedBox(height: 16.h,),
-        const BookRating(mainAxisAlignment: MainAxisAlignment.center,),
-        SizedBox(height: 30.h,),
-        const BookButton()
+    return CustomScrollView(
+      slivers: [
+        SliverFillRemaining(
+          hasScrollBody: false,
+          child:  Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              const CustomBookDetailsAppBar(),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: width * .28),
+                child: const CustomBookImage(),
+              ),
+              // SizedBox(
+              //   height: 5.h,
+              // ),
+              Text(
+                'The Jungle Book',
+                style: Styles.textStyle20.copyWith(
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(
+                height: 7.h,
+              ),
+              Opacity(
+                opacity: .7,
+                child: Text(
+                  'Rudyard Kip',
+                  style: Styles.textStyle18.copyWith(
+                      fontStyle: FontStyle.italic, fontWeight: FontWeight.w500),
+                ),
+              ),
+              SizedBox(
+                height: 16.h,
+              ),
+              const BookRating(
+                mainAxisAlignment: MainAxisAlignment.center,
+              ),
+              SizedBox(
+                height: 30.h,
+              ),
+              const BookButton(),
+              Expanded(
+                child: SizedBox(
+                  height: 50.h,
+                ),
+              ),
+              Align(alignment: Alignment.centerLeft,
+                child: Text(
+                  'You Can Also Like',
+                  style: Styles.textStyle14.copyWith(fontWeight: FontWeight.w600),
+                ),
+              ),
+              SizedBox(height: 30.h,),
+              const  SmallerListView()
+            ],
+          ) ,
+        )
       ],
     );
+
+
+
   }
 }
---
+++ lib/views/component/smaller_list_view.dart
@@ -0,0 +1,23 @@
+import 'package:booklyapp/views/component/list_view_item/custom_Book_Image.dart';
+import 'package:flutter/material.dart';
+
+class SmallerListView extends StatelessWidget {
+  const SmallerListView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return  Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 10.0,),
+      child: SizedBox(
+        height: MediaQuery.of(context).size.height * .11,
+        child: ListView.builder(
+          itemCount: 20,
+          scrollDirection: Axis.horizontal,
+          itemBuilder: (context, index) {
+            return  const CustomBookImage();
+          },
+        ),
+      ),
+    );
+  }
+}
