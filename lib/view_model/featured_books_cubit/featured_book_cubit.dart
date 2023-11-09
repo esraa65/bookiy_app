2c54e180753df1f8dc15698483d9f1e57cb14551
esraa65 <esraakhaled6565@gmail.com>
2023-11-09 16:40:57 +0200

    featured book cubit done

--- lib/view_model/featured_books_cubit/featured_book_cubit.dart
+++ lib/view_model/featured_books_cubit/featured_book_cubit.dart
@@ -1,9 +1,19 @@
 import 'package:bloc/bloc.dart';
 import 'package:booklyapp/models/book_model.dart';
-import 'package:meta/meta.dart';
+import 'package:booklyapp/models/repos/home_repo.dart';
 
 part 'featured_book_state.dart';
 
 class FeaturedBookCubit extends Cubit<FeaturedBookState> {
-  FeaturedBookCubit() : super(FeaturedBookInitial());
+  FeaturedBookCubit(this.homeRepo) : super(FeaturedBookInitial());
+  final HomeRepo homeRepo;
+  Future<void> getFeaturedBooks() async {
+    emit(FeaturedBookLoading());
+    var result = await homeRepo.fetchFeaturedBooks();
+    result.fold((failure) {
+      emit(FeaturedBookFailure(failure.errormessage));
+    }, (books) {
+      emit(FeaturedBookSuccess(books));
+    });
+  }
 }
--- lib/view_model/featured_books_cubit/featured_book_state.dart
+++ lib/view_model/featured_books_cubit/featured_book_state.dart
@@ -1,17 +1,18 @@
 part of 'featured_book_cubit.dart';
 
-@immutable
 abstract class FeaturedBookState {}
 
 class FeaturedBookInitial extends FeaturedBookState {}
+
 class FeaturedBookLoading extends FeaturedBookState {}
+
 class FeaturedBookFailure extends FeaturedBookState {
   final String errormessage;
   FeaturedBookFailure(this.errormessage);
 }
+
 class FeaturedBookSuccess extends FeaturedBookState {
+  final List<BookModel> books;
 
-final  List<BookModel> books;
-
-FeaturedBookSuccess(this.books);
+  FeaturedBookSuccess(this.books);
 }
